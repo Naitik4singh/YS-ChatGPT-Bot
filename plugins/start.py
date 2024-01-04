a2df65e2f5735988e3bf6352de6efef482e9269c
Naitik4singh <153117195+Naitik4singh@users.noreply.github.com>
2024-01-04 22:07:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+    await m.reply_photo(f"https://telegra.ph/file/c707ad3718d268afd9811.jpg",
         caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[Tᴇᴄʜ YS](https://t.me/@YS_BOTZ_01)**",
         reply_markup=InlineKeyboardMarkup(
                 [

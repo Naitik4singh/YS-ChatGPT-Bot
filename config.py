711eecdf8e8eaccab78d1b4c0671a8f0f91f2a4a
Naitik4singh <153117195+Naitik4singh@users.noreply.github.com>
2024-01-04 22:24:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
+API_ID = int(environ.get("API_ID", "28542813"))
+API_HASH = environ.get("API_HASH", "02ce7c339f7776844ff4ab03da338ccd")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6468449640:AAGWY7mbMdkREIJFQrkiD9XtgC6OfrMGv_o")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002015639009"))
+ADMINS = int(environ.get("ADMINS", "6064893774"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://yashsing2008:12345678yash@cluster0.j8n5cqz.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = environ.get("DB_NAME", "cluster0")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
